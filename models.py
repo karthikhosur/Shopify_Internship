4a4966cd21157f6d71b8fb39b0ae3fba0da7ed17
karthikhosur <rheuzaki@gmail.com>
2022-01-08 23:40:23 -0600

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from database import Base
-
-# Define To Do class inheriting from Base
-class Item(Base):
-    __tablename__ = 'item'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(256))
-    
